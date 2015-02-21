24a268c235bea5168a0abab08e98a29e19c0a7d6
jcollingosch <jcollingosch@gmail.com>
2015-02-20 23:41:03 -0700

    added eda and model diagnostics code

--- regression-project.R
+++ regression-project.R
@@ -1,11 +1,25 @@
 
 library("caret")
+library("ggplot2")
 
+#eda
+ggplot(mtcars, aes(x=factor(am), y=mpg, fill=factor(am))) +
+  geom_boxplot() +
+  guides(fill=guide_legend(title="Transmission")) +
+  labs(x="Transmission (0 = automatic, 1 = manual)",
+       title="MPG by Transmission")
+
+pairs(mtcars,pch=20)
+
+# fit candidate models
 set.seed(123)
 fit <- train(mpg~.,data = mtcars, method = "lmStepAIC")
 
-# fit <- regsubsets(mpg~.,data=mtcars)
-
+# choose final model
 lm_fit <- lm(mpg~wt+qsec+am,data=mtcars)
 
+# check model output
 summary(lm(mpg~cyl+hp+wt,data=mtcars))
+
+par(mfrow=c(2,2))
+plot(lm_fit)
